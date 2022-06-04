690d77760308c8f62c502ca34248b14ca7d6581c
taulant-coder <70835261+taulant-coder@users.noreply.github.com>
2022-06-04 22:45:29 +0200

    exploratory analysis project

---
+++ expl_1.R
@@ -0,0 +1,15 @@
+# Exploratory Analysis I
+
+# Taulant Elshani
+
+# Prevents histogram from printing in scientific notation
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+png(filename='plot1.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
---
+++ expl_3.R
@@ -0,0 +1,12 @@
+# Subset NEI data by Baltimore
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
